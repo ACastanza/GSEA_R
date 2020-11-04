c6bcb163a08c2b74d18e213a917d93e9aeb0637e
Anthony S. Castanza <acastanza@ucsd.edu>
2020-11-04 10:51:50 -0800

    nwGSEA

--- R/GSEA.get.local.network.R
+++ R/GSEA.get.local.network.R
@@ -25,18 +25,22 @@ GSEA.get.local.network <- function(global.netowrk, set, score.type = "strength",
  } else if (score.type == "tif") {
   # Topology Influence Factor adapted from Hung et al. PMID:20187943
   d <- distances(gene.set.graph)
-  set.expression.matrix <- expression.matrix[rownames(d),]
-  pcc <- stats::cor(t(set.expression.matrix), method="pearson", use="pairwise.complete.obs")
-  d <- d[rownames(pcc),rownames(pcc)]
-  f <- d/abs(pcc)
-  diag(f) <- NA
-  valid <- f <= -log(0.05)
-  f <- as.data.frame(t(f))
-  valid <- as.data.frame(t(valid))
-  tif <- unlist(Map(function(x, y) if (sum(y, na.rm = TRUE) > 0) 
-   mean(x[y], na.rm = TRUE) else NA, f, valid))
-  tif <- as.numeric(weighted.score.type) + exp(-tif)
-  gene.set.weight <- tif
+  set.expression.matrix <- expression.matrix[rownames(d), ]
+  if (length(set.expression.matrix) > 0) {
+   pcc <- stats::cor(t(set.expression.matrix), method = "pearson", use = "pairwise.complete.obs")
+   d <- d[rownames(pcc), rownames(pcc)]
+   f <- d/abs(pcc)
+   diag(f) <- NA
+   valid <- f <= -log(0.05)
+   f <- as.data.frame(t(f))
+   valid <- as.data.frame(t(valid))
+   tif <- unlist(Map(function(x, y) if (sum(y, na.rm = TRUE) > 0) 
+    mean(x[y], na.rm = TRUE) else NA, f, valid))
+   tif <- as.numeric(weighted.score.type) + exp(-tif)
+   gene.set.weight <- tif
+  } else {
+   gene.set.weight <- rep(as.numeric(weighted.score.type), length(set))
+  }
  }
  gene.set.weight <- gene.set.weight[set]
  names(gene.set.weight) <- set
